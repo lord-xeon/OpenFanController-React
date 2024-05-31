bd8a12a278a33161569210ca4bd33a57527ee7d5
lord-xeon <lord.xeon@deadlycomputer.com>
2024-05-31 15:51:09 -0400

    content editable

--- Software/js/src/FanItem.jsx
+++ Software/js/src/FanItem.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import CircularSlider from '@fseehawer/react-circular-slider';
 
 
 export default function FanItem(props){
+	let [fanName, setFanName] = useState(props.name);
+
+	function updateFanName(value){
+		setFanName(value.target?.innerText?.trim());
+	}
+
 	return (
 		<article className="fan-item">
 			<header>
 				<h4>Fan Index {props.id}</h4>
-				<h5 contentEditable>{props.name}</h5>
+				<h5 contentEditable onBlur={updateFanName}>{fanName}</h5>
 			</header>
+			
 			<CircularSlider
 				label="Target Speed"
 				max={2500}
